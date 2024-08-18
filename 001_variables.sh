83cdb45e9e46dc53089af6dfbf195f78aaf1ea9d
ajjupawar <ajinkyapawar110@gmail.com>
2024-08-18 17:21:00 +0530

    added new variables

---
+++ 001_variables.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+#Created variables
+
+username="Ajinkya"
+password="Pawar"
+host="google.com"
+city="Pune"
+
+# curl -v "https://www.$host"
+read aj
+echo "Entered value is" $username $password $aj
